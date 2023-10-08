3ece97e6a57fbf56e3ccaa44924e52b455554de2
safranek2 <safranek2@spsejecna.cz>
2023-10-08 15:56:36 +0200

    Change to firebase

--- chatroom/get_messages.php
+++
@@ -1,4 +0,0 @@
-<?php
-$chat_history = file_get_contents("messages.txt");
-echo nl2br($chat_history);
-?>
--- chatroom/script.js
+++ chatroom/script.js
@@ -1,32 +1,35 @@
 $(document).ready(function() {
-    var chat = $("#chat");
-    var messageInput = $("#message");
-    var sendButton = $("#send");
+    const chat = document.getElementById("chat");
+    const messageInput = document.getElementById("message");
+    const sendButton = document.getElementById("send");
 
-    function updateChat() {
-        $.ajax({
-            url: "messages.txt",
-            type: "GET",
-            success: function(data) {
-                chat.html(data);
-            }
-        });
-    }
+    const firebaseConfig = {
+        apiKey: "AIzaSyC6IvcL_jzTZvpT8Jg6AVdJEEn7u5Ay4FA",
+        authDomain: "chatroom-df69f.firebaseapp.com",
+        projectId: "chatroom-df69f",
+        storageBucket: "chatroom-df69f.appspot.com",
+        messagingSenderId: "98215758452",
+        appId: "1:98215758452:web:2a1c251fbe8ac6ccc29f21",
+        measurementId: "G-N5MEMETX4S"
+      };
 
-    setInterval(updateChat, 1000);
+    firebase.initializeApp(firebaseConfig);
 
-    sendButton.click(function() {
-        var message = messageInput.val();
+    sendButton.addEventListener("click", () => {
+        const message = messageInput.value;
         if (message !== "") {
-            $.ajax({
-                url: "send_message.php",
-                type: "POST",
-                data: { message: message },
-                success: function() {
-                    messageInput.val("");
-                    updateChat();
-                }
+            firebase.database().ref("messages").push({
+                text: message,
+                timestamp: firebase.database.ServerValue.TIMESTAMP
             });
+            messageInput.value = "";
         }
+    });
+
+    firebase.database().ref("messages").on("child_added", (snapshot) => {
+        const messageData = snapshot.val();
+        const messageDiv = document.createElement("div");
+        messageDiv.textContent = messageData.text;
+        chat.appendChild(messageDiv);
     });
 });
