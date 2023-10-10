cd83e33fc1a6e752d06460657f0e0c2f81cdefcd
safranek2 <safranek2@spsejecna.cz>
2023-10-10 17:06:41 +0200

    Update script.js

--- chatroom/script.js
+++ chatroom/script.js
@@ -1,16 +1,16 @@
-// Import Firebase a potřebné funkce
-var firebase = require('firebase/app');
-require('firebase/database');
+import firebase from "firebase/app";
+import "firebase/database";
 
-let firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyC6IvcL_jzTZvpT8Jg6AVdJEEn7u5Ay4FA",
     authDomain: "chatroom-df69f.firebaseapp.com",
+    databaseURL: "https://chatroom-df69f-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "chatroom-df69f",
     storageBucket: "chatroom-df69f.appspot.com",
     messagingSenderId: "98215758452",
     appId: "1:98215758452:web:2a1c251fbe8ac6ccc29f21",
     measurementId: "G-N5MEMETX4S"
-};
+  };
 
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
