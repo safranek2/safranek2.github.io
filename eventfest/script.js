a50fd31c1d99dc976d08e0e3443d7760c6e24dff
safranek2 <safranek2@spsejecna.cz>
2024-04-04 21:48:59 +0200

    Update

---
+++ eventfest/script.js
@@ -0,0 +1,6 @@
+window.onerror = function(message, source, lineno, colno, error) {
+    console.error('Chyba: ' + message);
+    console.error('Zdroj: ' + source);
+    console.error('Řádek: ' + lineno);
+    console.error('Sloupec: ' + colno);
+};
